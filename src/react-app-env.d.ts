59e8001ab64a8053257b7bbaec204343d92a5bae
Jerome Raffin <raffin.jerome@gmail.com>
2021-03-02 19:58:44 +0100

    chore: install welcome-ui and styled-components
    + module declaration for typescript

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module "@welcome-ui/core";
+declare module "@welcome-ui/box";
+declare module "@welcome-ui/input-text";
+declare module "@welcome-ui/date-picker";
+declare module "@welcome-ui/select";
+declare module "@welcome-ui/button";
+declare module "@welcome-ui/icons";
+declare module "@welcome-ui/text";
+declare module "@welcome-ui/stack";
